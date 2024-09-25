047a13313f665fca5b55e4f94332f67699b95d38
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2024-09-24 21:42:50 -0300

    Terminando funcionalidade de adcionar contato

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 
 
 def cli():
@@ -7,7 +9,6 @@ def cli():
           "3 - Adicionar contato\n" +
           "4 - Deletar um contato\n" +
           "5 - Atualizar um contato\n")
-
     option = 0
     while option == 0:
         option = int(input('Qual opção deseja?'))
@@ -19,7 +20,20 @@ def cli():
             print('Listando um contato específico...')
 
         if option == 3:
-            print('Adicionando um contato...')
+            print('Adicionando contato')
+            try:
+                conn = sqlite3.connect('contact_list')
+                cursor = conn.cursor()
+                cursor.execute("""
+                                      INSERT INTO contato VALUES
+                                      (1, 'Teste 1', '5199345123', 'alexandreteste1@gmail.com'),
+                                      (2, 'Teste 2', '5555532321', 'alexandreteste2@gmail.com')
+                                   """)
+                conn.commit()
+                conn.close()
+            except Exception as e:
+                print(f'Registro já existe  \n{e}')
+            print('Contato adicionado')
 
         if option == 4:
             print('Deletando um contato...')
