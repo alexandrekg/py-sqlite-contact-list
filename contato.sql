b7de140a8f71020a02187dae351a145e32e057ad
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2024-10-01 22:54:15 -0300

    Deletando arquivos desnecessários

--- contato.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE contato (
-    id integer primary key,
-    name text NOT NULL,
-    email text,
-    phone text
-);
-
-INSERT INTO contato (name, email, phone) VALUES ('Teste1', 'teste1@gmail.com', '5199993939393');
